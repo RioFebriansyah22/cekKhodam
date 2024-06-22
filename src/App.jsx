4edfa05a22d3a36da06a4ea6c173cd03b43e5b53
Nama Anda <Riofebriansyah775@gmail.com>
2024-06-22 16:02:20 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import InputForm from './Components/CardKodam';
+import emyu from './Images/emyu.jpg'
+import rumme from './Images/rumme.jpg'
+
+function App() {
+  const [nameInput, setNameInput] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [khodam, setKhodam] = useState(null);
+
+  const khodams = [
+    { name: 'harimau himalaya' },
+    { name: 'nyi blorong' },
+    { name: 'keris sakti' },
+    { name: 'kucing isriwil' },
+    { name: 'kucing oyen' },
+    { name: 'sapi kurban' },
+    { name: 'manusia kepala buaya' },
+    { name: 'buaya lgbt' },
+    { name: 'sapu lidi' },
+    { name: 'tehyung' },
+    { name: 'jongkok bts' },
+    { name: 'karina aespa' },
+    { name: 'boboiboy solar' },
+    { name: 'boboiboy daun' },
+    { name: 'kipas angin' },
+    { name: 'pocong' },
+    { name: 'rawa rontek' },
+    { name: 'harimau putih' },
+    { name: 'Bocil epep' },
+    { name: 'Alok' },
+    { name: 'Goat Messi' },
+
+    { name: 'Rummenigge',img: rumme, decs: 'kamu adalah titisan Rumme' },
+    { name: 'emyu',img: emyu, decs: 'yahaha fans emyu' },
+    // tambahkan kodam
+  ];
+
+  const cekKhodam = () => {
+    if (!nameInput) {
+      alert('GAK BOLEH KOSONG!😡');
+      return;
+    }
+    setLoading(true);
+    setTimeout(() => {
+      const randomKhodam = khodams[Math.floor(Math.random() * khodams.length)];
+      setKhodam(randomKhodam);
+      setLoading(false);
+    }, 500);
+  };
+
+  const resetForm = () => {
+    setNameInput('');
+    setKhodam(null);
+  };
+
+  return (
+    <div className="bg-red-600 min-h-screen flex flex-col justify-center items-center text-center">
+      <div className="card  border-2 bg-red-600 rounded-lg p-8 max-w-lg  w-full shadow-lg backdrop-blur-lg relative shadow-slate-200">
+        <h1 className="text-white text-3xl mb-4 font-bold">Cek Khodam</h1>
+        <p className="text-white mb-4">Masukkan nama untuk mengetahui khodam</p>
+        
+        <InputForm
+          nameInput={nameInput}
+          setNameInput={setNameInput}
+          cekKhodam={cekKhodam}
+          loading={loading}
+          reset={resetForm}
+          kodam={khodam} // Prop ini harus disesuaikan dengan nama yang benar (kodam vs khodam)
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/CardKodam.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const InputForm = ({ nameInput, setNameInput, cekKhodam, loading, reset, kodam }) => {
+  return (
+    <div className="mb-4 relative">
+      <input
+        type="text"
+        value={nameInput}
+        onChange={(e) => setNameInput(e.target.value)}
+        placeholder="Ketik nama kamu di sini"
+        className="bg-transparent text-white rounded-full border-2 focus:border-white border-white py-2 px-4 text-center w-80 mb-4"
+      />
+      <button
+        onClick={cekKhodam}
+        className="bg-green-500 mr-3 text-white py-2 px-6 rounded-full text-lg hover:bg-green-600 transform hover:-translate-y-1 hover:shadow-lg transition-all duration-300"
+        disabled={loading}
+      >
+        {loading ? 'Loading...' : 'Cek Khodam'}
+      </button>
+      {loading && (
+        <div className="loading-spinner border-4 border-white border-solid rounded-full w-10 h-10 animate-spin absolute top-[25%] left-[45%]"></div>
+      )}
+      <button
+        onClick={reset}
+        className="bg-gray-500 text-white py-2 px-6 rounded-full text-lg mt-4 hover:bg-gray-600 transform hover:-translate-y-1 hover:shadow-lg transition-all duration-300"
+      >
+        Reset
+      </button>
+      {kodam && (
+        <div className="text-green-500 mt-4 text-2xl font-bold">
+          <h2 className="text-xl mb-2">Khodam: {kodam.name}</h2>
+          <img className='w-[30%] mx-auto' src={kodam.img} alt="" />
+          <h2 className="text-xl mb-2"> {kodam.decs}</h2>
+
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default InputForm;
